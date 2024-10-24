7d281201aee00303ac2b761cc078dc83fadec0cb
Jonathan Putney <jputney@noverant.com>
2024-10-24 07:25:42 -0400

    Working on exports

--- src/ScormAgain.ts
+++ src/ScormAgain.ts
@@ -2,4 +2,8 @@ import AICCImpl from "./AICC";
 import Scorm12Impl from "./Scorm12API";
 import Scorm2004Impl from "./Scorm2004API";
 
-export { AICCImpl, Scorm12Impl, Scorm2004Impl };
+const Scorm12API = Scorm12Impl;
+const Scorm2004API = Scorm2004Impl;
+const AICC = AICCImpl;
+
+export { Scorm12API, Scorm2004API, AICC };
