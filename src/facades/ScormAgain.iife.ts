86856b6409716e6270860efc62af628f097c76c0
Jonathan Putney <jputney@noverant.com>
2025-04-30 13:24:26 -0400

    Rollback

--- src/facades/ScormAgain.iife.ts
+++
@@ -1,6 +0,0 @@
-import AICC from "../AICC";
-import Scorm12API from "../Scorm12API";
-import Scorm2004API from "../Scorm2004API";
-
-// Directly assign to the global namespace
-export { AICC, Scorm12API, Scorm2004API };
