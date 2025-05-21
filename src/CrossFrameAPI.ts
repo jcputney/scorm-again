fa080bab51be4e782fddf0f852a4b1b24c6cb8e2
Jonathan Putney <42720634+jcputney@users.noreply.github.com>
2025-05-21 10:25:43 -0400

    fix: improve error code parsing in CrossFrameAPI (#961)

--- src/CrossFrameAPI.ts
+++ src/CrossFrameAPI.ts
@@ -144,8 +144,9 @@ export default class CrossFrameAPI {
   /** Capture and cache SCORM errors */
   private _capture(method: string, err: any) {
     console.error(`CrossFrameAPI ${method} error:`, err);
-    const code = (/(\d{3})/.exec(err.message) || [])[1] || global_errors.GENERAL;
-    this._lastError = String(code);
+    const match = /\b(\d{3})\b/.exec(err.message);
+    const code = match ? match[1] : String(global_errors.GENERAL);
+    this._lastError = code;
     this._cache.set(`error_${code}`, err.message);
   }
 }
